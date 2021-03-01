6cc7c481f2b7aa48d42355fd2160c0520f992b37
Matthew Bolton <matthewwbolton@gmail.com>
2021-03-01 11:15:50 -0500

    completed course section on creating command line scripts

--- exercises/ex1.js
+++ exercises/ex1.js
@@ -2,10 +2,63 @@
 
 "use strict";
 
-// My first Node.js program
+var util = require("util");
+var path = require("path");
+var fs = require("fs");
 
-process.stdout.write('Hello World\n');
+var getStdin = require("get-stdin");
 
-// console.log('Hello World')
+var args = require("minimist")(process.argv.slice(2), {
+  boolean: ["help", "in"],
+  string: ["file"],
+});
 
-console.error('Ooops')
+var BASE_PATH = path.resolve(process.env.BASE_PATH || __dirname);
+
+if (process.env.HELLO) {
+  console.log(process.env.HELLO);
+}
+
+if (args.help) {
+  printHelp();
+} else if (args.in || args._.includes("-")) {
+  getStdin().then(processFile).catch(error);
+} else if (args.file) {
+  fs.readFile(
+    path.join(BASE_PATH, args.file),
+    function onContents(err, contents) {
+      if (err) {
+        error(err.toString());
+      } else {
+        processFile(contents.toString());
+      }
+    }
+  );
+} else {
+  error("Incorrect Usage", true);
+}
+
+//***************************
+
+function processFile(contents) {
+  contents = contents.toUpperCase();
+  process.stdout.write(contents);
+}
+
+function error(msg, includeHelp = false) {
+  console.error(msg);
+  if (includeHelp) {
+    console.log("");
+    printHelp();
+  }
+}
+
+function printHelp() {
+  console.log("ex1 usage: ");
+  console.log(" ex1.js --file={FILENAME}");
+  console.log("");
+  console.log("--help                 print this help");
+  console.log("--file={FILENAME}      process the file");
+  console.log("--in                   process stdin");
+  console.log("");
+}
